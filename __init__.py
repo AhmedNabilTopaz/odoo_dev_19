45b5f0a72b3cd69584272a8f442fc8dfce485946
OmarFanta <112676648+omerfanta@users.noreply.github.com>
2026-04-16 18:44:52 +0200

    update

---
+++ __init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from . import journal_print_wizard
+from . import journal_summary_wizard
---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Watan Journal Summary Print',
+    'version': '19.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Print account totals (Debit/Credit/Balance) filtered by company, date range, and eZee accounts',
+    'depends': ['account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/journal_print_wizard_views.xml',
+        'wizard/journal_summary_wizard_views.xml',
+        'report/journal_print_report.xml',
+        'report/journal_summary_report.xml',
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
